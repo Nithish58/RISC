900b3c4a8e6009a0cbf761216976aa56d238a8a2
xinjiezeng <xinjiezeng@gmail.com>
2019-10-08 13:39:20 -0400

    add mapIntro and mapGraph for model

--- src/main/java/com6441/team7/risc/api/model/MapIntro.java
+++ src/main/java/com6441/team7/risc/api/model/MapIntro.java
@@ -1,4 +1,14 @@
 package com6441.team7.risc.api.model;
 
 public class MapIntro {
+
+    private String mapIntro;
+
+    public String getMapIntro() {
+        return mapIntro;
+    }
+
+    public void setMapIntro(String mapIntro) {
+        this.mapIntro = mapIntro;
+    }
 }
