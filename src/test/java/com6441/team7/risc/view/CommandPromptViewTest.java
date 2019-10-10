0d05700a6f0ca7f74570ab1a89d069c1e2375c51
binsarphutapeaENCS <b_hutape@encs.concordia.ca>
2019-10-10 00:52:58 -0400

    Added test class for CommandPromptView
    Co-authored-by: bikash-jaiswal <bjjaiswal@gmail.com>
    Co-authored-by: Tirathraj <ramburntirathraj@gmail.com>
    Co-authored-by: Nithish58 <n_chadal@live.concordia.ca>

---
+++ src/test/java/com6441/team7/risc/view/CommandPromptViewTest.java
@@ -0,0 +1,26 @@
+package com6441.team7.risc.view;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class CommandPromptViewTest {
+
+	@BeforeClass
+	public void initClass() {}
+	
+	@Before
+	public void beginTest() {}
+	
+	@After
+	public void endTest() {}
+	
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
