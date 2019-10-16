bf8885650c2b117052744278db11198c3ab53c12
binsarphutapeaENCS <b_hutape@encs.concordia.ca>
2019-10-15 20:30:15 -0400

    Update to fortify test
    Co-authored-by: bikash-jaiswal <bjjaiswal@gmail.com>
    Co-authored-by: Tirathraj <ramburntirathraj@gmail.com>
    Co-authored-by: Nithish58 <n_chadal@live.concordia.ca>

--- src/test/java/com6441/team7/risc/controller/FortifyGameControllerTest.java
+++ src/test/java/com6441/team7/risc/controller/FortifyGameControllerTest.java
@@ -2,8 +2,12 @@ package com6441.team7.risc.controller;
 
 import static org.junit.Assert.*;
 
+import java.io.File;
+import java.net.URI;
+import java.nio.charset.StandardCharsets;
 import java.util.Optional;
 
+import org.apache.commons.io.FileUtils;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -15,19 +19,26 @@ import com6441.team7.risc.api.model.Country;
 import com6441.team7.risc.api.model.GameState;
 import com6441.team7.risc.api.model.MapService;
 import com6441.team7.risc.api.model.Player;
+import com6441.team7.risc.view.CommandPromptView;
 
 public class FortifyGameControllerTest {
-	MapService mapService;
+	static MapService mapService;
+	static private CommandPromptView cmdView;
 	Player player1;
 	fortifyGameController fortifyController;
 	fortifyGameController skippedFortifyController;
 	Optional<Country> fromCountry;
 	Optional<Country> toCountry;
 	GameState nextState;
-	String fortifyCommand1, fortifyCommand2;
-
+	String fortifyCommand1, fortifyCommand2, file;
+	static MapLoaderController mapCtrl;
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
+		mapService = new MapService();
+		mapCtrl = new MapLoaderController(mapService);
+		cmdView = new CommandPromptView(mapCtrl, new GameController(mapCtrl, mapService));
+		mapCtrl.setView(cmdView);
+
 	}
 
 	@AfterClass
@@ -36,16 +47,19 @@ public class FortifyGameControllerTest {
 
 	@Before
 	public void setUp() throws Exception {
+		URI uri = getClass().getClassLoader().getResource("ameroki.map").toURI(); 
+		file = FileUtils.readFileToString(new File(uri), StandardCharsets.UTF_8);
 		player1 = new Player("Player_1");
-		mapService = new MapService();
 		fortifyController = new fortifyGameController(player1, mapService);
 		skippedFortifyController = new fortifyGameController(player1, mapService);
-		//fortifyController.fortify();
+		//the following two lines are to be changed
+		mapCtrl.parseFile(file);
 		nextState = GameState.REINFORCE;
 		
-		fortifyCommand1 = "fortify country_A country_B 1";
+		
+		fortifyCommand1 = "fortify Siberia Worrick 1";
 		fortifyCommand2 = "fortify none";
-		fortifyController.readCommand(fortifyCommand1);
+		//fortifyController.readCommand(fortifyCommand1);
 		skippedFortifyController.readCommand(fortifyCommand2);
 	}
 
