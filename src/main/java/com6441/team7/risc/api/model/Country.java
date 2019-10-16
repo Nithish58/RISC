587c851c0d1f746eecc0a93e6aae57e784b0ff92
Bikash <bjjaiswal@gmail.com>
2019-10-15 22:43:31 -0400

    added javadoc comment in Country class

--- src/main/java/com6441/team7/risc/api/model/Country.java
+++ src/main/java/com6441/team7/risc/api/model/Country.java
@@ -10,99 +10,213 @@ import static com6441.team7.risc.api.RiscConstants.WHITESPACE;
  * This class stores country information
  */
 public class Country {
+
+    /**
+     * The id of country in the map
+     */
     private final Integer id;
+
+    /**
+     * The name of the country
+     */
     private final String name;
+
+    /**
+     * The Id of continent in which this country is located
+     */
     private Integer continentIdentifier;
+
+    /**
+     * The name of continent where this country is located
+     */
     private String continentName;
+
+    /**
+     * The X Cordinate of country's location in map
+     */
     private Integer coordinateX;
+
+    /**
+     * The y cordinate of country's location in map
+     */
     private Integer coordinateY;
+
+    /**
+     * The Player which owns this country
+     */
     private Player player;
+
+    /**
+     * the initial soldiers of this country is set to 0
+     */
     private int soldiers = 0;
 
+    /**
+     * The constructor to initialize country with parameter id and countryName
+     * @param id The id of country which is being initialized
+     * @param countryName The countryName of country which is being initialized
+     */
     public Country(Integer id, String countryName) {
         this.id = id;
         this.name = countryName.toLowerCase(Locale.CANADA);
     }
 
+    /**
+     * The constructor to initialize the country with parameter id, countryName and continentName
+     * @param id The id of country which is being initialized
+     * @param countryName The countryName of country which is being initialized
+     * @param continentName The name of continent where the country is loacated
+     */
     public Country(Integer id, String countryName, String continentName){
         this.id = id;
         this.name  = countryName.toLowerCase(Locale.CANADA);
         this.continentName = continentName;
     }
 
+    /**
+     * The constructor to initialize the country with parameter id, countryName and continentName
+     * @param id The id of country which is being initialized
+     * @param countryName The countryName of country which is being initialized
+     * @param continentId The Id of continent where the country is loacated
+     */
     public Country(Integer id, String countryName, Integer continentId){
         this.id = id;
         this.name = countryName.toLowerCase(Locale.CANADA);
         this.continentIdentifier = continentId;
     }
 
-
+    /**
+     * Getter method to get Id of country
+     * @return Id of country
+     */
     public Integer getId() {
         return id;
     }
 
+    /**
+     * Getter method to get Name of country
+     * @return Name of country.
+     */
     public String getCountryName() {
         return name;
     }
 
-
+    /**
+     * setter method to set continent Id to country
+     * @param continentIdentifier
+     * @return the country
+     */
     public Country setContinentIdentifier(Integer continentIdentifier) {
         this.continentIdentifier = continentIdentifier;
         return this;
     }
 
+    /**
+     * getter methods to get continent Id of country
+     * @return continent Id
+     */
     public Integer getContinentIdentifier() {
         return continentIdentifier;
     }
 
-
+    /**
+     * To set continent name
+     * @param continentName continent name
+     * @return country
+     */
     public Country setContinentName(String continentName){
         this.continentName = continentName.toLowerCase(Locale.CANADA);
         return this;
     }
 
+    /**
+     *To get continent name of country
+     * @return continent name
+     */
     public String getContinentName() {
         return continentName;
     }
 
+    /**
+     * To get x cordinate of country's location
+     * @return x xordinate of country's location
+     */
     public Integer getCoordinateX() {
         return coordinateX;
     }
 
+    /**
+     * To set x coordinates of country's location
+     * @param coordinateX value of X coordinate
+     * @return country
+     */
     public Country setCoordinateX(Integer coordinateX) {
         this.coordinateX = coordinateX;
         return this;
     }
 
+    /**
+     *To get y cordinate of country's location
+     * @return value of Y coordinate
+     */
     public Integer getCoordinateY() {
         return coordinateY;
     }
 
+    /**
+     * To set y cordinate of country's location
+     * @param coordinateY value of y coordinate
+     * @return country
+     */
     public Country setCoordinateY(Integer coordinateY) {
         this.coordinateY = coordinateY;
         return this;
     }
 
+    /**
+     * To get player who occupies the country
+     * @return Player whoc occupies the country
+     */
     public Player getPlayer() {
         return player;
     }
 
+    /**
+     *To set the ownership of the country to player
+     * @param player transfering ownership to the player
+     */
     public void setPlayer(Player player) {
         this.player = player;
     }
 
+    /**
+     *To get the number of armies associated with this country
+     * @return number of soldiers
+     */
     public Integer getSoldiers() {
         return soldiers;
     }
 
+    /**
+     * To set the number of armies to the the country
+     * @param soldiers
+     */
     public void setSoldiers(Integer soldiers) {
         this.soldiers = soldiers;
     }
 
+    /**
+     * To add additional armies to the country
+     * @param number the numbers to add to existing armies
+     */
     public void addSoldiers(int number){
         this.soldiers += number;
     }
 
+    /**
+     * The reduce the armies of the country by given number
+     * @param number the number to reduce from present armies.
+     */
     public void removeSoldiers(int number){
         this.soldiers -= number;
     }
@@ -126,6 +240,10 @@ public class Country {
         return Objects.hash(id);
     }
 
+    /**
+     * To print country information
+     * @return id name continentIdentifier coordinateX CoordinateY.
+     */
     @Override
     public String toString() {
         return id + WHITESPACE +
