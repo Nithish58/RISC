75f10b420c15cabd643fcc04ec669f1a079d76cb
Bikash <bjjaiswal@gmail.com>
2019-10-15 22:47:33 -0400

    added javadoc comment in Mapgraph

--- src/main/java/com6441/team7/risc/api/model/MapGraph.java
+++ src/main/java/com6441/team7/risc/api/model/MapGraph.java
@@ -4,12 +4,24 @@ package com6441.team7.risc.api.model;
  * This class stores graph information
  */
 public class MapGraph {
+
+    /**
+     * Set mapGraph to empty string.
+     */
     private String mapGraph = "";
 
+    /**
+     * TO get mapGraph
+     * @return mapgraph
+     */
     public String getMapGraph() {
         return mapGraph;
     }
 
+    /**
+     * To set value to mapGraph
+     * @param mapGraph the value to set
+     */
     public void setMapGraph(String mapGraph) {
         this.mapGraph = mapGraph;
     }
