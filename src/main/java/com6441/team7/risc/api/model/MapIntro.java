ae18956f04c56c1629801ae55efb62e3f6a29c56
Bikash <bjjaiswal@gmail.com>
2019-10-15 22:50:12 -0400

    added javadoc comment in MapIntro

--- src/main/java/com6441/team7/risc/api/model/MapIntro.java
+++ src/main/java/com6441/team7/risc/api/model/MapIntro.java
@@ -5,12 +5,23 @@ package com6441.team7.risc.api.model;
  */
 public class MapIntro {
 
+    /**
+     * Set mapIntro to empty string.
+     */
     private String mapIntro = "";
 
+    /**
+     * To get mapIntro
+     * @return mapIntro
+     */
     public String getMapIntro() {
         return mapIntro;
     }
 
+    /**
+     * To set value to mapIntro
+     * @param mapIntro the value to set
+     */
     public void setMapIntro(String mapIntro) {
         this.mapIntro = mapIntro;
     }
