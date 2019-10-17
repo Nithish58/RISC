82143e9fb68f00ce5fdce0aa63cdc11d21d96ffa
binsarphutapeaENCS <b_hutape@encs.concordia.ca>
2019-10-16 23:06:03 -0400

    Added ignore annotation to unused test classes
    Co-authored-by: bikash-jaiswal <bjjaiswal@gmail.com>
    Co-authored-by: Tirathraj <ramburntirathraj@gmail.com>
    Co-authored-by: Nithish58 <n_chadal@live.concordia.ca>
    Co-authored-by: XinjieZeng <xinjiezeng@gmail.com>

--- src/test/java/com6441/team7/risc/api/exception/ContinentParsingExceptionTest.java
+++ src/test/java/com6441/team7/risc/api/exception/ContinentParsingExceptionTest.java
@@ -6,29 +6,31 @@ import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
+import org.junit.Ignore;
 import org.junit.Test;
 
+@Ignore
 public class ContinentParsingExceptionTest {
 
-//	@BeforeClass
-//	public static void setUpBeforeClass() throws Exception {
-//	}
-//
-//	@AfterClass
-//	public static void tearDownAfterClass() throws Exception {
-//	}
-//
-//	@Before
-//	public void setUp() throws Exception {
-//	}
-//
-//	@After
-//	public void tearDown() throws Exception {
-//	}
-//
-//	@Test
-//	public void test() {
-//		fail("Not yet implemented");
-//	}
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
 
 }
--- src/test/java/com6441/team7/risc/api/model/GameStateTest.java
+++ src/test/java/com6441/team7/risc/api/model/GameStateTest.java
@@ -6,34 +6,36 @@ import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
+import org.junit.Ignore;
 import org.junit.Test;
 
 /**
  * Test class for GameState enum
  *
  */
+@Ignore
 public class GameStateTest {
 	GameState gameState;
 
-//	@BeforeClass
-//	public static void setUpBeforeClass() throws Exception {
-//	}
-//
-//	@AfterClass
-//	public static void tearDownAfterClass() throws Exception {
-//	}
-//
-//	@Before
-//	public void setUp() throws Exception {
-//	}
-//
-//	@After
-//	public void tearDown() throws Exception {
-//	}
-//
-//	@Test
-//	public void test() {
-//		fail("Not yet implemented");
-//	}
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
 
 }
