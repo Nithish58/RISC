57f272123d6da22319d2eb5f9451efca08b83a53
binsarphutapeaENCS <b_hutape@encs.concordia.ca>
2019-10-16 22:10:22 -0400

    Remove unneccessary lines
    Co-authored-by: bikash-jaiswal <bjjaiswal@gmail.com>
    Co-authored-by: Tirathraj <ramburntirathraj@gmail.com>
    Co-authored-by: Nithish58 <n_chadal@live.concordia.ca>
    Co-authored-by: XinjieZeng <xinjiezeng@gmail.com>

--- src/main/java/com6441/team7/risc/App.java
+++ src/main/java/com6441/team7/risc/App.java
@@ -6,24 +6,12 @@ import com6441.team7.risc.controller.MapLoaderController;
 import com6441.team7.risc.view.CommandPromptView;
 
 /**
- * Hello world!
+ * This is the main class.
  *
  */
 public class App {
     public static void main( String[] args ) {
-    	
-    	/*
-    	 * NEED TO CHANGE ORDER:
-    	 * NOTES AND JOEY SPECIFICALLY SAID THAT:
-    	 * CREATE MODEL FIRST
-    	 * THEN CREATE VIEW
-    	 * THEN CONTROLLER
-    	 * 
-    	 * EVEN THOUGH IT DOESN'T MATTER, LET'S ADHERE TO WHAT HE SAYS
-    	 * BECAUSE HE LIKES TO REMOVE MARKS FOR UNNECESSARY THINGS...
-    	 */
-    	
-    	
+    		
         MapService mapService = new MapService();
         
         MapLoaderController mapLoaderController = new MapLoaderController(mapService);
