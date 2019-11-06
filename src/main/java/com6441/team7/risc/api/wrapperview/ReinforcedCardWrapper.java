74090954d844ec69b5594867c16460bae63c76e8
xinjiezeng <xinjiezeng@gmail.com>
2019-11-05 19:38:59 -0500

    add javadoc

--- src/main/java/com6441/team7/risc/api/wrapperview/ReinforcedCardWrapper.java
+++ src/main/java/com6441/team7/risc/api/wrapperview/ReinforcedCardWrapper.java
@@ -5,27 +5,60 @@ import com6441.team7.risc.api.model.Player;
 
 import java.util.List;
 
+/**
+ * a self-defined wrapper for reinforcement stage
+ */
 public class ReinforcedCardWrapper {
+
+    /**
+     * a list of cards for the player
+     */
     private List<Card> cards;
+
+    /**
+     * the current player
+     */
     private Player player;
 
+
+    /**
+     * constructor
+     * @param player
+     * @param cards
+     */
     public ReinforcedCardWrapper(Player player, List<Card> cards) {
         this.player = player;
         this.cards = cards;
     }
 
+    /**
+     * get the list of cards
+     * @return
+     */
     public List<Card> getCards() {
         return cards;
     }
 
+    /**
+     * set the list of cards
+     * @param cards
+     */
     public void setCards(List<Card> cards) {
         this.cards = cards;
     }
 
+    /**
+     * get current player
+     * @return
+     */
     public Player getPlayer() {
         return player;
     }
 
+    /**
+     * set current player
+     * @param player
+     */
     public void setPlayer(Player player) {
         this.player = player;
     }
