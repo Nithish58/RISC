c56c70b643f9a6b297ef6060f7c28cf2ee3fdc33
Tirathraj <t_rambur@encs.concordia.ca>
2019-11-24 21:13:14 -0500

    Added some Javadocs

--- src/test/java/com6441/team7/risc/controller/StartupGameControllerTest.java
+++ src/test/java/com6441/team7/risc/controller/StartupGameControllerTest.java
@@ -50,6 +50,9 @@ public class StartupGameControllerTest {
 	  */
 	 StartupGameController startupGameController;
 	 
+	 /**
+	  * Controller for loading game
+	  */
 	 LoadGameController loadGameController;
 	 
 	 /**
